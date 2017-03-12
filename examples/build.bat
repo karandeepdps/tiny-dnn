7f03f7acb945fd0bb345b5d572dd4fa2b0995b11
Karandeep <karandeepdps@gmail.com>
2017-03-12 15:22:06 +0530

    mnist test

--- examples/build.bat
+++ examples/build.bat
@@ -1,1 +1,1 @@
-g++ -std=c++11 -Wall -Werror -Wextra -pedantic -I ../ -O3 main.cpp -o main
+g++ -std=c++11 -DCNN_USE_OMP=0 -I /Users/Karan/desktop/opencv/tiny-dnn/tiny-dnn/ -Wall -Werror -Wextra -pedantic -O3 main.cpp -o main 
